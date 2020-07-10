f5aaab2d9c59feb1699db9e74c6212980784a240
IsaacShelton <isaac.a.shelton@gmail.com>
2020-07-09 22:35:53 -0500

    Fixed release.bat

--- release.bat
+++ release.bat
@@ -1,7 +1,7 @@
 del "NAME.txt" >nul 2>&1
 mkdir C:\Users\%USERNAME%\Desktop\GCGRELEASEOUT
-copy *.dll C:\%USERNAME%\isaac\Desktop\GCGRELEASEOUT
-copy *.exe C:\%USERNAME%\isaac\Desktop\GCGRELEASEOUT
+copy *.dll C:\Users\%USERNAME%\Desktop\GCGRELEASEOUT
+copy *.exe C:\Users\%USERNAME%\Desktop\GCGRELEASEOUT
 mkdir C:\Users\%USERNAME%\Desktop\GCGRELEASEOUT\assets
 xcopy /s /e assets C:\Users\%USERNAME%\Desktop\GCGRELEASEOUT\assets
 
