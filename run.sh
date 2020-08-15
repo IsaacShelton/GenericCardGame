45b9a7079904239eacec8d0c07e56076478a708a
IsaacShelton <isaac.a.shelton@gmail.com>
2020-08-14 20:13:50 -0500

    Added lots of stuff

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(rm NAME.txt || true) && (./main & ./main)
-#(rm NAME.txt || true) && (./main --host=localhost & ./main --host=localhost)
+#(rm NAME.txt || true) && (./main & ./main)
+(rm NAME.txt || true) && (./main --host=192.168.1.141 & ./main --host=192.168.1.141)
